6aac947a1ca318ebfa6756f3a719349a5d6feafd
RamiroCyber <ramiro.dr@gmail.com>
2022-11-09 23:25:32 -0300

    conexao com o firestore database concluida

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
-  FirebaseFirestore.instance.collection("col").doc("doc").set(({"texto": "ramiro"}));
+  FirebaseFirestore.instance
+      .collection("col")
+      .doc("doc")
+      .set(({"texto": "ramiro"}));
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
